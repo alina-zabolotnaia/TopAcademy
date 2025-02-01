8c2172ba11d9c8f0ac4dcd4909620fb15bdfcfc8
alina-zabolotnaia <alina.zabolotnaia@gmail.com>
2025-02-01 16:54:19 +0300

    [HW-1] fix some output messages

--- home_work_1.py
+++ home_work_1.py
@@ -1,7 +1,7 @@
 # @author: alina.zabolotnaia@gmail.com
 
 def get_speed(i_distance, i_time):
-    print(f'Рассчитаем скорость движения по параметрам:\n')
-    print(f'- расстояние (км.)   : {i_distance}')
-    print(f'- время в пути (час) : {i_time}')
+    print(f'Рассчет скорости движения по входным параметрам:\n')
+    print(f'- (S) расстояние (км.)   : {i_distance}')
+    print(f'- (T) время в пути (час) : {i_time}')
     return i_distance/i_time
--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import home_work_1, home_work_2
 
 if __name__ == '__main__':
     print('- - - - - - - - - - - - - HOME WORK #1 - - - - - - - - - - - - - ')
-    v_distance = float(input('Введите расстояние:'))
-    v_time = float(input('Введите время в пути:'))
-    print(f'Скорость движения = {home_work_1.get_speed(v_distance, v_time)} км/ч')
+    v_distance = float(input('Введите значение расстояния (км): '))
+    v_time = float(input('Введите затраченное время в пути (часы): '))
+    print(f'Расчетная скорость движения = {home_work_1.get_speed(v_distance, v_time)} км/ч')
 
-    print('- - - - - - - - - - - - - HOME WORK #2 - - - - - - - - - - - - - ')
+    print('\n- - - - - - - - - - - - - HOME WORK #2 - - - - - - - - - - - - - ')
     print(f'Вызываем метод расчета miles to km {home_work_2.miles_to_km_converter()}')
